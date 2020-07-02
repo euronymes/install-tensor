a91808186318adc71a6a3d10222822c01ba2a993
euronymes <39071143+euronymes@users.noreply.github.com>
2020-07-02 11:22:56 +0200

    Create partinstall.sh

---
+++ partinstall.sh
@@ -0,0 +1,8 @@
+sudo apt install python3.6 python3.6-pip3 python3.6-dev python-dev
+sudo apt install nvidia-cuda-toolkit
+sudo dpkg -i libcudnn7-dev_7.6.5.32-1+cuda10.1_amd64.deb
+sudo dpkg -i libcudnn7_7.6.5.32-1+cuda10.1_amd64.deb
+sudo dpkg -i libcudnn7-doc_7.6.5.32-1+cuda10.1_amd64.deb
+pip3 install tensorflow
+pip install pyQt5
+pip3 install pyqtgraph
