f842409cffaa0caa5d62648ce7d25fc69981c24c
euronymes <39071143+euronymes@users.noreply.github.com>
2020-07-03 17:11:07 +0200

    Add files via upload

---
+++ pip_install.sh
@@ -0,0 +1,18 @@
+pip3 install pyinstaller
+pip3 install astor
+python -m pip install --user numpy scipy matplotlib ipython jupyter pandas sympy nose
+pip3 install numba
+pip3 install --upgrade pypeaks
+pip3 install smooth
+pip3 install opencv-python
+pip3 install qimage2ndarray
+pip3 install scikit-image
+pip3 install pyueye
+pip3 install pyModbusTCP
+pip3 install python-snap7
+pip3 install detecta
+pip3 install --user Cython
+pip3 install --user contextlib2
+pip3 install --user pillow
+pip3 install --user lxml
+pip3 install --user tf_slim
