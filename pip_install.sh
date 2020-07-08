01a3de495882d8fc801da95b07899442899ce323
euronymes <39071143+euronymes@users.noreply.github.com>
2020-07-08 09:37:06 +0200

    Update pip_install.sh

--- pip_install.sh
+++ pip_install.sh
@@ -1,6 +1,16 @@
+#python3 install
+sudo apt install python3.6 python3.6-pip3 python3.6-dev python-dev python3-pip
+#passage python systeme 2.7->3.6
+sudo update-alternatives --install /usr/bin/python python /usr/bin/python2.7 1
+sudo update-alternatives --install /usr/bin/python python /usr/bin/python3.6 2
+sudo update-alternatives  --set python /usr/bin/python3.6
+pip3 install pyqtgraph
+pip3 install tensorflow
+pip3 install PyQt5
+pip3 install 
 pip3 install pyinstaller
 pip3 install astor
-python -m pip install --user numpy scipy matplotlib ipython jupyter pandas sympy nose
+python -m pip install --user numpy scipy matplotlib ipython jupyter pandas sympy nose 
 pip3 install numba
 pip3 install --upgrade pypeaks
 pip3 install smooth
