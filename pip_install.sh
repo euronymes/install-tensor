651057cc7ba134341cebd6e503828d097b94732a
euronymes <39071143+euronymes@users.noreply.github.com>
2020-08-14 11:51:00 +0200

    Update pip_install.sh

--- pip_install.sh
+++ pip_install.sh
@@ -4,5 +4,6 @@ sudo apt install python3.6 python3.6-pip3 python3.6-dev python-dev python3-pip
 sudo update-alternatives --install /usr/bin/python python /usr/bin/python2.7 1
 sudo update-alternatives --install /usr/bin/python python /usr/bin/python3.6 2
 sudo update-alternatives  --set python /usr/bin/python3.6
+pip3 install --upgrade pip
 pip3 install -r requirement.txt
 
