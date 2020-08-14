113039467fad0a5e9b2bc6d98949c09a80e3057c
euronymes <39071143+euronymes@users.noreply.github.com>
2020-08-14 11:46:18 +0200

    Update pip_install.sh

--- pip_install.sh
+++ pip_install.sh
@@ -4,25 +4,5 @@ sudo apt install python3.6 python3.6-pip3 python3.6-dev python-dev python3-pip
 sudo update-alternatives --install /usr/bin/python python /usr/bin/python2.7 1
 sudo update-alternatives --install /usr/bin/python python /usr/bin/python3.6 2
 sudo update-alternatives  --set python /usr/bin/python3.6
-pip3 install pyqtgraph
-pip3 install tensorflow
-pip3 install PyQt5
-pip3 install 
-pip3 install pyinstaller
-pip3 install astor
-python -m pip install --user numpy scipy matplotlib ipython jupyter pandas sympy nose 
-pip3 install numba
-pip3 install --upgrade pypeaks
-pip3 install smooth
-pip3 install opencv-python
-pip3 install qimage2ndarray
-pip3 install scikit-image
-pip3 install pyueye
-pip3 install pyModbusTCP
-pip3 install python-snap7
-pip3 install detecta
-pip3 install --user Cython
-pip3 install --user contextlib2
-pip3 install --user pillow
-pip3 install --user lxml
-pip3 install --user tf_slim
+pip3 install -r requirement.txt
+
